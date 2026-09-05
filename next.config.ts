fbd7b6cd3e766f94751418c0a2c824675737e907
abrarhusayn <m.abrarhusayn@gmail.com>
2026-09-05 17:19:06 +0530

    workflow

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   allowedDevOrigins: ['sharpness-easeful-ripping.ngrok-free.dev'],
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  allowedDevOrigins: ["sharpness-easeful-ripping.ngrok-free.dev"],
 };
-
-module.exports = {
-  allowedDevOrigins: ['sharpness-easeful-ripping.ngrok-free.dev'],
-}
 
 export default nextConfig;
